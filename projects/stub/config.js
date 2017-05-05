2b9cd772d88eccef46df1b8ffee6d8f066b1377e
palegrow <bynast@gmail.com>
2017-05-05 15:58:56 +0300

    Add example with nested project

---
+++ projects/stub/config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  builder: {
+    levels: [
+      'node_modules/pale-blocks/blocks',
+      'blocks',
+      'projects/stub/blocks'
+    ],
+    techMap: {
+      css: ['post.css', 'css'],
+    }
+  },
+
+  bundles: 'bundles',
+
+  browserslist: [
+    "last 2 versions"
+  ],
+};
